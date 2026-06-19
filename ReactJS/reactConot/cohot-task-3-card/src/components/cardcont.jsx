d0f9b4d5db5ac92630d76e82f00f1fae1787c458
Subhodip Chatterjee <subhodip31xd@gmail.com>
2026-06-19 20:51:28 +0530

    card-task-3-cohot

--- ReactJS/reactConot/cohot-task-3-card/src/components/cardcont.jsx
+++ ReactJS/reactConot/cohot-task-3-card/src/components/cardcont.jsx
@@ -4,9 +4,9 @@ import Profile from './profile';
 
 const cardcont = () => {
   return (
-    <div className=' h-full w-full px-16 py-16 bg-amber-300 rounded-4xl'>
-        <div className=" relative h-100 w-75 bg-amber-700 rounded-4xl px-2 py-2">
-            <div className='w-full h-[100px] bg-amber-900 rounded-3xl px-1.5 py-1.5 flex items-start justify-end'>
+    <div className=' h-full w-full px-16 py-16 bg-amber-300 rounded-4xl '>
+        <div className=" relative h-100 w-75 bg-amber-700 rounded-4xl overflow-hidden">
+            <div className='w-[95%] h-[100px] bg-amber-900 rounded-3xl px-1.5 py-1.5 flex items-start justify-end mt-2 ml-2'>
                 <Button />
             </div>
          <Profile />
@@ -17,7 +17,7 @@ const cardcont = () => {
      
 
 
-         <div className=' flex items-center justify-between py-4 px-3 bg-neutral-500 rounded-2xl'>
+         <div className=' flex items-center justify-between py-4 px-3 bg-neutral-500 rounded-2xl m-2'>
             <div className=''>
                 <h1>79.6k</h1>
                 <p>followres</p>
@@ -32,7 +32,11 @@ const cardcont = () => {
             </div>
 
          </div>
-
+        <div className='w-full flex item-center justify-between px-6 text-1xl bg-gray-700 pb-10 pt-1'>
+            <i class="ri-instagram-line"></i>
+        <i class="ri-flutter-line"></i> 
+        <i class="ri-chat-smile-ai-3-line"></i>
+        </div>
 
         </div>
     </div>
