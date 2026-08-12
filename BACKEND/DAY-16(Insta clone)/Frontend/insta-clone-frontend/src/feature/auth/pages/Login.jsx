0fd5e4639b3336673dd6da92aaf7a6cc2d1821d5
Subhodip Chatterjee <subhodip31xd@gmail.com>
2026-08-12 23:56:31 +0530

    frontend implementation  api and file structure

--- BACKEND/DAY-16(Insta clone)/Frontend/insta-clone-frontend/src/feature/auth/pages/Login.jsx
+++ BACKEND/DAY-16(Insta clone)/Frontend/insta-clone-frontend/src/feature/auth/pages/Login.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../style/form.scss"
 import {Link} from "react-router"
+import axios from 'axios';
 
 const Login = () => {
+    const [Username, setUsername] = useState("")
+    const [Password, setPassword] = useState("")
+      const handleSubmit = (e)=>{
+    e.preventDefault();
+    axios.post('http://localhost:3000/api/user/auth/login',{
+      Username,
+      Password
+      
+    },{
+        withCredentials:true
+      }).then(res=>{
+      console.log(res.data);
+      
+    })
+  }
   return (
     <div>
       <main>
         <div className="form-cont">
           <h1>login</h1>
-          <form action="">
-            <input type="text" name="Username" placeholder="Enter your username" />
-            <input type="password" name="Password" placeholder="Enter your password" />
+          <form action="" onSubmit={handleSubmit}>
+            <input onInput={(e)=>{setUsername(e.target.value)}} type="text" name="Username" placeholder="Enter your username" />
+            <input onInput={(e)=>{setPassword(e.target.value)}} type="password" name="Password" placeholder="Enter your password" />
             <button type="submit">login</button>
           </form>
           <p>Don't have a account ? <Link to={"/register"}>register</Link></p>
--- BACKEND/DAY-16(Insta clone)/Frontend/notes.md
+++ BACKEND/DAY-16(Insta clone)/Frontend/notes.md
@@ -6,3 +6,21 @@ install react router DOM
 npm i sass
 form using apin in frontent of backend
 npm axios
+now when we r sending a post request through /register the the register the user a set a token but in frontend the token is not set bcz axion dont set cookies so we pass a extra obj in axion {withcredentials:true}
+and is cors({
+    credentials:true,
+    origin:"http//lc5173"
+})
+day-22 
+now today we will understand folder structure
+which is in four layer 
+=> ui show ui and navigate
+=> hook 
+manage state and api layer 
+=> state state manage krrna
+loading
+user login or 
+posts 
+err
+
+=> api backend se communicate krna
